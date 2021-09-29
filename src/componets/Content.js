fef5fc6c8248e541cb6124ed0e6b924d3efd5adc
amitjamwal <bikujamwal@gmail.com>
2021-09-29 09:39:30 +0530

    init

---
+++ src/componets/Content.js
@@ -0,0 +1,15 @@
+
+function Content(){
+    return(
+        <>
+        <div id="clockContainer">
+            <div id="hour"></div>
+            <div id="minute"></div>
+            <div id="second"></div>
+        </div>
+
+        </>
+    )
+}
+
+export default Content;
